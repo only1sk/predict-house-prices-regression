01888fbbac5d8530f1b213c79b3ece05055bcb1a
only1stk <skoti21@gmail.com>
2024-01-10 17:54:59 +0000

    Update configuration settings

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
 import os
 
 # This will get the absolute path to the project root directory.
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'C:/Users/chame/OneDrive/Desktop/Test_Env_Data_Analysis/For_Porfolio/House Prices - Advanced Regression/house-prices-advanced-regression-techniques/'))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '_enter_full_path_to_project_root_folder'))
